e3d6bb8fbb015234a3decb492946936f827edb0e
GauravKGoyal <GauravGoya@gmail.com>
2015-07-03 17:13:51 +1200

    Making use of JavascriptDateTimeConverter to get correct datetime in in milliseconds instead of utc format

--- Dentist/Controllers/TryController.cs
+++ Dentist/Controllers/TryController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Dentist.Models;
 using Kendo.Mvc.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Dentist.Controllers
 {
@@ -17,6 +19,26 @@ namespace Dentist.Controllers
             return View();
         }
 
+        public JsonResult DateTime()
+        {
+            var obj = new SomeObject();
+            obj.DateTimeLocal = System.DateTime.Now;
+            obj.DateTimeUtc = System.DateTime.UtcNow;
+            obj.DateTimeLocalSerilized = JsonConvert.SerializeObject(obj.DateTimeLocal);
+            obj.DateTimeLocalISOSerilized = JsonConvert.SerializeObject(obj.DateTimeLocal, new IsoDateTimeConverter());
+            obj.DateTimeLocalJavaScriptSerilized = JsonConvert.SerializeObject(obj.DateTimeLocal, new JavaScriptDateTimeConverter());
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class SomeObject
+    {
+        public DateTime DateTimeLocal { get; set; }
+        public DateTime DateTimeUtc { get; set; }
+
+        public string DateTimeLocalSerilized { get; set; }
+        public string DateTimeLocalISOSerilized { get; set; }
+        public string DateTimeLocalJavaScriptSerilized { get; set; }
     }
 
 
