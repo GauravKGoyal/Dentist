8352a17de38dbbe7967de95c86abe86d219dd1f6
GauravKGoyal <GauravGoya@gmail.com>
2015-09-12 13:10:44 +1200

    commit missing files

---
+++ Dentist/Models/Tags/IModelWithId.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dentist.Models.Tags
+{
+    public interface IModelWithId
+    {
+        [Editable(false)]
+        int Id { get; set; }
+    }
+}
